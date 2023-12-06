d81c4014682379697e16bf2c9ea748007d07a760
İbrahim Uludağ <ibrahimuludag@gmail.com>
2023-12-07 01:11:52 +0300

    Initialize

---
+++ src/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DataAnnotationsSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotationsSample;
+
+public class Student
+{
+    [Required]
+    public Guid Id { get; set; }
+
+    [Length(1, 20)]
+    [Required]
+    public string FirstName { get; set; }
+
+    [Length(1, 20)]
+    [Required]
+    public string LastName { get; set; }
+
+
+    [Range(15, 25, MinimumIsExclusive = true, MaximumIsExclusive = true)]
+    public int Age { get; set; }
+
+    [AllowedValues("Freshman", "Sophomore", "Junior", "Senior")]
+    public string Grade { get; set; }
+
+    [DeniedValues("123456", "qwerty")]
+    public string Password { get; set; }
+
+    [Base64String]
+    public string Hash { get; set; }
+}
